bbc5651d0054af774c998127f406702c10a6efda
MichaelFisher1997 <contact@michaelfisher.tech>
2022-04-28 00:22:36 +0100

    made progress on product tests, images faail to load

--- client/src/pages/product_Interface.js
+++ client/src/pages/product_Interface.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import productTest from "./productTest.json";
 
-//const productArray = [];
+
+
+
 
 
 export default function getProducts()
 {
-    
+
     return (
         <div className="products">
+            
             <br></br>
 
             {
                 productTest.map(productTest => {
                     return (
                         <div className='productCard' ley={productTest.productID}>
-                            {/*image link here */}
+
+                            <img src={productTest.icon}/>
+                            <br/>
+                            
                             <p><strong>{productTest.prodName}</strong></p>
                             <p>{productTest.prodDescription}</p>
                             <p>Price: {productTest.price}</p>
+                            <button>Add</button>
                             <p></p>
                         </div>
                     )
