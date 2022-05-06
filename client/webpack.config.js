47bf0af024d1257461548f649cd6f6817cad1ae9
MichaelFisher1997 <contact@michaelfisher.tech>
2022-05-06 06:20:54 +0100

    zill function not working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
           assert: require.resolve('assert'),
           
           http: require.resolve('stream-http'),
+          account: require.resolve('@zilliqa-js/account'),
           https: require.resolve('https-browserify'),
           os: require.resolve('os-browserify/browser'),
           stream: require.resolve('stream-browserify'),
